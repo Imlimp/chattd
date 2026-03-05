6fb91750c8f778ddc45f03e7fde7cb4e2af7dccd
Linus Bensryd <linne01lampa@gmail.com>
2026-03-05 22:44:03 +0100

    Add server starting

--- main.go
+++ main.go
@@ -1,4 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
 func main() {
+	port := ":5012"
+	listner, err := net.Listen("tcp4", port)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listner.Close()
+
+	fmt.Printf("Opened server on port: %s.\n", port[1:])
+
+	for {
+		connection, err := listner.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		go handleConnection(connection)
+	}
+}
+
+func handleConnection(connection net.Conn) {
+	fmt.Printf("Connection: %s\n", connection.RemoteAddr().String())
 }
